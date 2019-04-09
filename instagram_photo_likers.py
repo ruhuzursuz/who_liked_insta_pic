f7ef62cc1231b99ab81485b3b29ff271ad2140c2
ruhuzursuz <48116342+ruhuzursuz@users.noreply.github.com>
2019-04-09 19:03:35 +0300

    Update instagram_photo_likers.py

--- instagram_photo_likers.py
+++ instagram_photo_likers.py
@@ -9,13 +9,8 @@ def get_likers(link):
         x_i_gis = hashlib.md5(text.encode()).hexdigest()
         return x_i_gis
 
-    def attempt_number(number):
-        if number%50 == 0:
-            return number//50
-        else:
-            return (number//50) + 1
-
     who_liked = []
+    end_cursor = "first_attempt"
     query_hash_like = "e0f59e4a1c8d78d0161873bc2ee7ec44"
     short_code = link.split("/")[4]
     user_agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.127"
@@ -27,14 +22,12 @@ def get_likers(link):
 
     rhx_gis = re.findall('rhx_gis":"(.*?)"',r.text)[0]
     number_of_likes = int(re.findall('userInteractionCount":"(.*?)"',r.text)[0])
-    loop_number = attempt_number(number_of_likes)
 
-    for loop in range(loop_number):
-        if loop == 0:
+    while True:
+        if end_cursor == "first_attempt":
             query_variable = '{"shortcode":"' + short_code + '","include_reel":true,"first":50}'
         else:
             query_variable = '{"shortcode":"' + short_code + '","include_reel":true,"first":50,"after":"' + end_cursor + '"}'
-            
         payload = {"query_hash": query_hash_like,"variables" : query_variable}
         x_instagram_gis = calculate_x_instagram_gis(rhx_gis, query_variable)
         header = {"user-agent": user_agent, "x-instagram-gis": x_instagram_gis}
@@ -45,8 +38,10 @@ def get_likers(link):
         users = json_response["data"]["shortcode_media"]["edge_liked_by"]["edges"]
         for user in users:
             who_liked.append(user["node"]["username"])
-    
+        if end_cursor is None:
+            break
+
     print(who_liked)
-    ### or return who_liked
+    return who_liked
 
 get_likers(paste_link_here)
