cd17aa4d69f22d8ee2d97a883d663b87536d76d0
ruhuzursuz <48116342+ruhuzursuz@users.noreply.github.com>
2019-04-09 19:20:23 +0300

    Update instagram_photo_likers.py

--- instagram_photo_likers.py
+++ instagram_photo_likers.py
@@ -16,31 +16,27 @@ def get_likers(link):
     user_agent = "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36 OPR/58.0.3135.127"
     header = {"user-agent": user_agent}
 
-    s = requests.Session()
-    s.headers.update(header)
-    r = s.get(link)
+    session = requests.Session()
+    session.headers.update(header)
+    response = session.get(link)
 
-    rhx_gis = re.findall('rhx_gis":"(.*?)"',r.text)[0]
+    rhx_gis = re.findall('rhx_gis":"(.*?)"',response.text)[0]
 
     while True:
         if end_cursor == "first_attempt":
             query_variable = '{"shortcode":"' + short_code + '","include_reel":true,"first":50}'
         else:
             query_variable = '{"shortcode":"' + short_code + '","include_reel":true,"first":50,"after":"' + end_cursor + '"}'
-           
+        payload = {"query_hash": query_hash_like,"variables" : query_variable}
         x_instagram_gis = calculate_x_instagram_gis(rhx_gis, query_variable)
         header = {"user-agent": user_agent, "x-instagram-gis": x_instagram_gis}
-        s.headers.update(header)
-        
-        payload = {"query_hash": query_hash_like,"variables" : query_variable}
-        r = s.get("https://www.instagram.com/graphql/query/?", params = payload)
-        json_response = r.json()
+        session.headers.update(header)
+        json_response = session.get("https://www.instagram.com/graphql/query/?", params = payload).json()
         end_cursor = json_response["data"]["shortcode_media"]["edge_liked_by"]["page_info"]["end_cursor"]
         users = json_response["data"]["shortcode_media"]["edge_liked_by"]["edges"]
-        
         for user in users:
             who_liked.append(user["node"]["username"])
         if end_cursor is None:
             break
-            
+        
     return who_liked
